3561e21f3e242a79b084cbe8859e0e9b78ac9247
Alef Santos <alefdeoliveirasantos@gmail.com>
2022-05-15 16:54:55 -0300

    acho q tá finalizado

--- main.cpp
+++ main.cpp
@@ -1,27 +1,23 @@
-#include<iostream>
-
 #include"afd.hpp"
 
 using namespace std;
 
-void mostra_se_foi_aceito(bool aceito) {
-  aceito
-    ? cout << "Aceito" << endl
-    : cout << "Não aceito" << endl;
-  return;
-}
-
 int main() {
 
-  AFD afd1("afd1.txt", true);
-  AFD afd2("afd2.txt", true);
-  AFD afd3("afd3.txt", true);
-  AFD afd4("afd4.txt", true);
-  AFD afd5("afd5.txt", true);
-  AFD afd6("afd6.txt", true);
+  AFD afd_1a = AFD("./afd1a.txt", true);
+  afd_1a.testa_arquivo_inteiro(true);
 
-  afd1.testa_arquivo_inteiro(true);
+  AFD afd_1b = AFD("./afd1b.txt", true);
+  afd_1b.testa_arquivo_inteiro(true);
 
+  AFD afd_1c = AFD("./afd1c.txt", true);
+  afd_1c.testa_arquivo_inteiro(true);
+
+  AFD afd_2a = AFD("./afd2a.txt", true);
+  afd_2a.testa_arquivo_inteiro(true);
+
+  AFD afd_2b = AFD("./afd2b.txt", true);
+  afd_2b.testa_arquivo_inteiro(true);
 
   return 0;
 }
